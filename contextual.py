30c4bf89fedaca1945d858b0d275f021778eeea5
dyns <dyns@users.noreply.github.com>
2016-12-09 18:16:14 -0700

    u

--- contextual.py
+++ contextual.py
@@ -1,10 +1,10 @@
 
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from os import listdir
 from os.path import isfile, join
-import c_search as csearch
 
 NOTES_PATH = 'notes'
 
